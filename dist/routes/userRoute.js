08a7f4eca79a1352eab9330c37b51c6866cc6827
unknown <dawid.pios00@gmail.com>
2024-05-15 18:06:31 +0200

    Added user auth route

--- dist/routes/userRoute.js
+++ dist/routes/userRoute.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userController_1 = require("../controllers/userController");
 const router = express_1.default.Router();
-router.get('/getUser', userController_1.getUser).get('/authUser', userController_1.authUser).post('/addUser', userController_1.addUser);
+router.get('/getUser', userController_1.getUser).post('/authUser', userController_1.authUser).post('/addUser', userController_1.addUser);
 exports.default = router;
 //# sourceMappingURL=userRoute.js.map
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -2,6 +2,6 @@ import express from 'express'
 import { addUser, getUser, authUser } from '../controllers/userController'
 const router = express.Router()
 
-router.get('/getUser', getUser).get('/authUser', authUser).post('/addUser', addUser)
+router.get('/getUser', getUser).post('/authUser', authUser).post('/addUser', addUser)
 
 export default router
